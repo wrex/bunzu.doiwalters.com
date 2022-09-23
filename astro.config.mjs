9117adc4ee83370e4a3e4f08d2a67e85051840f9
Rex Walters <653208+wrex@users.noreply.github.com>
2022-09-23 11:44:35 -0700

    add mdx and svelte support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
-// import remarkDirective from "remark-directive";
-// import netlify from "@astrojs/netlify/functions";
+import svelte from "@astrojs/svelte"; // import netlify from "@astrojs/netlify/functions";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-  ],
+  integrations: [// Enable Preact to support Preact JSX components.
+  preact(), // Enable React for the Algolia search component.
+  react(), svelte(), mdx()],
   site: `https://bunzu.doiwalters.com`,
   // adapter: netlify(),
-  markdown: {
-    // remarkPlugins: [remarkDirective],
-    // extendDefaultPlugins: true,
-  },
+  markdown: {// extendDefaultPlugins: true,
+  }
 });
---
+++ src/components/Admonition.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let title = "Admonition";
+</script>
+
+<section class="admonition">
+  <header class="title">{title}</header>
+  <div class="content">
+    <slot />
+  </div>
+</section>
+
+<style>
+
+  .admonition {
+    padding: 15px;
+    margin-bottom: 21px;
+    border-left: 10px solid transparent;
+    /* border-color: blue; */
+    border-color: var(--theme-text-alert);
+    /* background-color: #f6fcff; */
+    background-color: var(--theme-code-inline-bg);
+  }
+
+  .admonition .title {
+    margin: 0;
+    text-transform: uppercase;
+    padding-left: 3px;
+    border: 1px solid;
+    border-style: hidden hidden solid;
+    /* color: blue; */
+    color: var(--theme-text-alert);
+    border-color: var(--theme-text-alert);
+    /* border-color: blue; */
+  }
+
+  .admonition .content {
+    color: var(--theme-text-light);
+    padding-left: 0.75em;
+    padding-right: 0.75em;
+    padding-top: 1.5em;
+    margin-left: 0;
+    border-left: 0;
+    border-top: 0;
+    min-height: 0;
+  }
+</style>
