e10968f2461a1adc257e4f301d7e3a1339a80c6f
Rex Walters <653208+wrex@users.noreply.github.com>
2022-09-25 13:27:00 -0700

    bring back more menu, github, mdx everywhere

--- src/config.ts
+++ src/config.ts
@@ -32,9 +32,9 @@ export const KNOWN_LANGUAGES = {
 
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/wrex/bunzu.doiwalters.com`;
+export const GITHUB_EDIT_URL = `https://github.com/wrex/bunzu.doiwalters.com/tree/main`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://github.com/wrex/bunzu.doiwalters.com/issues`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
