343c399141c44088ed14f13d0d205bfd3f85f0a2
Rex Walters <653208+wrex@users.noreply.github.com>
2022-09-27 16:14:36 -0700

    add Svelte component for figures

---
+++ src/components/Figure.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  let counter = 0;
+</script>
+<script>
+      export let caption;
+
+      counter += 1;
+      let figureId = 'fig' + counter;
+
+</script>
+
+<figure id={eltId}>
+  <slot />
+  <figcaption><a href="#{figureId}">Figure {counter}.</a> {caption}</figcaption>
+</figure>    
+
+<style>
+  figure {
+    font-size: smaller;
+    border: thin var(--theme-code-text) solid;
+    padding: 0.5em;
+  }
+</style>
