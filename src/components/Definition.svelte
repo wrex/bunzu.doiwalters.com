e97de121c9e47cf470c8e76fe5bb4a243733d789
Rex Walters <653208+wrex@users.noreply.github.com>
2022-10-04 20:36:06 -0700

    A definition component

---
+++ src/components/Definition.svelte
@@ -0,0 +1,34 @@
+<dl>
+  <dt><slot name="term"/></dt>
+  <dd><slot /></dd>
+</dl>
+
+<style>
+  dl {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: stretch;
+    border: solid var(--theme-code-inline-bg);
+    border-width: 1px 1px 0 0;
+  }
+  dt {
+    flex-basis: 20%;
+    padding: 1em;
+    background: var(--theme-code-inline-bg);
+    text-align: right;
+    color: var(--theme-code-inline-text);
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+  dd {
+    flex-basis: 70%;
+    flex-grow: 1;
+    margin: 0;
+    padding: 1em;
+    border-bottom: 1px solid var(--theme-code-inline-bg);
+    display: flex;
+    align-items: center;
+  }
+
+</style>
