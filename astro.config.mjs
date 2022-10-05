5ec0267e74f88c47f3822d19f5dfb87cbc53b29a
Rex Walters <653208+wrex@users.noreply.github.com>
2022-10-04 18:33:47 -0700

    install remark-ruby plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
+import remarkRuby from "remark-ruby";
 
 // import netlify from "@astrojs/netlify/functions";
 
@@ -10,7 +11,14 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 
 export default defineConfig({
-  integrations: [preact(), react(), svelte(), mdx()],
+  integrations: [
+    preact(),
+    react(),
+    svelte(),
+    mdx({
+      remarkPlugins: [remarkRuby],
+    }),
+  ],
   site: `https://bunzu.doiwalters.com`,
   // adapter: netlify(),
 });
