9a355d9be3484c122aaf87f221e25e5cdb622dd2
Rex Walters <653208+wrex@users.noreply.github.com>
2022-10-09 16:44:55 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import remarkRuby from "remark-ruby";
-
-// import netlify from "@astrojs/netlify/functions";
+import remarkRuby from "remark-ruby"; // import netlify from "@astrojs/netlify/functions";
 
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     preact(),
@@ -18,7 +17,7 @@ export default defineConfig({
     mdx({
       remarkPlugins: [remarkRuby],
     }),
+    sitemap(),
   ],
-  site: `https://bunzu.doiwalters.com`,
-  // adapter: netlify(),
+  site: `https://bunzu.doiwalters.com`, // adapter: netlify(),
 });
